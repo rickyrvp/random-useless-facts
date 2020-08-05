791c531719ed07d26aaf55dd22380e5677712216
rickyrvp <ricky_rvp@hotmail.com>
2020-08-06 00:56:21 +0100

    Initial commit

---
+++ src/main/java/com/ricky/randomuselessfacts/RandomUselessFactsApplication.java
@@ -0,0 +1,13 @@
+package com.ricky.randomuselessfacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomUselessFactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomUselessFactsApplication.class, args);
+	}
+
+}
